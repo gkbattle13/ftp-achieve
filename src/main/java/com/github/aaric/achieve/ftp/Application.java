bee75f9a7bb5f8ac735d7560926032002ea9d356
aaric <vipaaric@gmail.com>
2018-12-10 13:54:31 +0800

    0.0.1-SNAPSHOT init

---
+++ src/main/java/com/github/aaric/achieve/ftp/Application.java
@@ -0,0 +1,21 @@
+package com.github.aaric.achieve.ftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Launcher.
+ *
+ * @author Aaric, created on 2018-12-10T13:49.
+ * @since 0.0.1-SNAPSHOT
+ */
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Main
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
